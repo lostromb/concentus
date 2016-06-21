7a6314191a8ce23a6b4212a7d849616669a3c787
Logan Stromberg <loganstromberg@gmail.com>
2016-06-21 16:09:32 -0700

    The demo project is looking really snazzy now. I think we might be ready to go public

--- CSharp/ConcentusDemo/AudioWorker.cs
+++ CSharp/ConcentusDemo/AudioWorker.cs
@@ -12,18 +12,25 @@ namespace ConcentusDemo
     {
         private const int BUFFER_LENGTH_MS = 200;
 
-        private readonly string INPUT_FILE = @"Butterfly.raw";
+        private readonly InputFileDef[] InputFiles = new InputFileDef[]
+        {
+            new InputFileDef("Blunderbuss.raw", "\"Blunderbuss\" by Henrik José, Creative Commons license"),
+            new InputFileDef("Spacecut.raw", "\"Spacecut\" by Ercola/Fairlight, Creative Commons license"),
+            new InputFileDef("Jurgen.raw", "\"Jurgen: A Comedy of Justice\". Librivox recording"),
+        };
 
         private volatile bool _running = false;
 
         // The NAudio waveout device
         private NAudioPlayer _player;
 
-        // The input file stream
-        private FileStream _inputFileStream;
+        // The actual codec objects
+        private IOpusCodec _currentCodec;
+        private ConcentusCodec _concentus;
+        private NativeOpusCodec _opus;
 
-        // The actual codec object
-        private IOpusCodec _codec;
+        // The currently playing file
+        private InputFileDef _currentMusic;
 
         // Codec params that can be modified by other threads
         private readonly Mutex _codecParamLock = new Mutex();
@@ -34,17 +41,22 @@ namespace ConcentusDemo
         
         public AudioWorker()
         {
-            _codec = new ConcentusCodec();
+            _concentus = new ConcentusCodec();
+            _opus = new NativeOpusCodec();
+            _currentCodec = _concentus;
             _player = new NAudioPlayer();
+            _currentMusic = InputFiles[0];
         }
 
         public void Run(object dummy)
         {
-            // Amount to read from the file at a time (this equals 2ms)
-            int readSize = 4 * 48000 / 1000;
-            byte[] inputSamples = new byte[readSize];
+            foreach (var inputStream in InputFiles)
+            {
+                inputStream.Initialize();
+            }
+            
             _running = true;
-            _inputFileStream = new FileStream(INPUT_FILE, FileMode.Open);
+            
             _player.Start();
 
             while (_running)
@@ -59,27 +71,20 @@ namespace ConcentusDemo
                 _codecParamLock.WaitOne();
                 if (_codecParamChanged)
                 {
-                    _codec.SetBitrate(_bitrate);
-                    _codec.SetComplexity(_complexity);
-                    _codec.SetFrameSize(_frameSize);
+                    _currentCodec.SetBitrate(_bitrate);
+                    _currentCodec.SetComplexity(_complexity);
+                    _currentCodec.SetFrameSize(_frameSize);
                     _codecParamChanged = false;
                 }
+
+                AudioChunk inputPcm = _currentMusic.ReadChunk();
                 _codecParamLock.ReleaseMutex();
 
-                // Read from the input file
-                if (_inputFileStream.Position >= _inputFileStream.Length - readSize)
-                {
-                    // Loop if necessary
-                    _inputFileStream.Seek(0, SeekOrigin.Begin);
-                }
-                int bytesRead = _inputFileStream.Read(inputSamples, 0, readSize);
-                AudioChunk inputPcm = new AudioChunk(inputSamples, 48000);
-
                 // Run the opus encoder and decoder
-                byte[] compressedFrame = _codec.Compress(inputPcm);
+                byte[] compressedFrame = _currentCodec.Compress(inputPcm);
                 if (compressedFrame != null && compressedFrame.Length > 0)
                 {
-                    AudioChunk decompressed = _codec.Decompress(compressedFrame);
+                    AudioChunk decompressed = _currentCodec.Decompress(compressedFrame);
 
                     // Pipe the output to the audio device
                     _player.QueueChunk(decompressed);
@@ -111,9 +116,32 @@ namespace ConcentusDemo
             _codecParamLock.ReleaseMutex();
         }
 
+        public void UpdateCodec(int codec)
+        {
+            _codecParamLock.WaitOne();
+            if (codec == 0)
+                _currentCodec = _concentus;
+            else
+                _currentCodec = _opus;
+            _codecParamChanged = true;
+            _codecParamLock.ReleaseMutex();
+        }
+
+        public InputFileDef UpdateInputFile(int newFileIndex)
+        {
+            if (newFileIndex < InputFiles.Length && newFileIndex >= 0)
+            {
+                _codecParamLock.WaitOne();
+                _currentMusic = InputFiles[newFileIndex];
+                _codecParamLock.ReleaseMutex();
+            }
+
+            return _currentMusic;
+        }
+
         public CodecStatistics GetStatistics()
         {
-            return _codec.GetStatistics();
+            return _currentCodec.GetStatistics();
         }
 
         public void Stop()
---
+++ CSharp/ConcentusDemo/InputFileDef.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcentusDemo
+{
+    public class InputFileDef
+    {
+        public string FileName;
+        public string Attribution;
+        private FileStream _inputFileStream;
+
+        // Amount to read from the file at a time (this equals 2ms)
+        private const int readSize = 4 * 48000 / 1000;
+        private readonly byte[] inputSamples = new byte[readSize];
+
+        public InputFileDef(string fileName, string attribution)
+        {
+            FileName = fileName;
+            Attribution = attribution;
+        }
+
+        public void Initialize()
+        {
+            _inputFileStream = new FileStream(FileName, FileMode.Open);
+        }
+
+        public AudioChunk ReadChunk()
+        {
+            // Read from the input file
+            if (_inputFileStream.Position >= _inputFileStream.Length - readSize)
+            {
+                // Loop if necessary
+                _inputFileStream.Seek(0, SeekOrigin.Begin);
+            }
+            int bytesRead = _inputFileStream.Read(inputSamples, 0, readSize);
+            return new AudioChunk(inputSamples, 48000);
+        }
+    }
+}
