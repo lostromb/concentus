c8942565c83d58d0acecc4494501d964ada2e27b
lostromb <loganstromberg@gmail.com>
2024-05-25 11:34:23 -0700

    junk

---
+++ NewHotness/HellaUnsafe/Common/StructPtr.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HellaUnsafe.Common
+{
+    internal class StructPtr<T> where T : struct
+    {
+        internal T Value { get; set; }
+    }
+}
